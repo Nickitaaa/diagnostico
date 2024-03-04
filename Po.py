d1746abeca86a7b5cfe9a1905364a9f523950b6d
Nickitaaa <161756503+Nickitaaa@users.noreply.github.com>
2024-03-04 11:18:06 -0300

    Create Po.py

---
+++ Po.py
@@ -0,0 +1,16 @@
+## crea una clase llamda vehiculo capsa de procesar la informacion basica d los autos en una consecionaria. Debera tener constructor y los atributos del vehiculo serán: patente, goma, marca, modelo, kilometraje. Crear metodos (accesos) getter y (modifiacion) setter. mopstrar por pantalla al menos 1 atrubuto y modificar el kilometrajes
+
+class Vehiculo ():
+  def __init__(self, patente, goma, marca, modelo, kilometraje):
+    self.patente = patente
+    
+  
+patente
+goma
+marca
+modelo
+kilometraje
+
+
+
+ 
